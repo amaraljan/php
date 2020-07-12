5fede59cc6df4736c211d969bd83092367cbbe51
Janilson do Amaral <amaraljan@gmail.com>
2020-07-12 08:52:35 -0300

    segundo teste

---
+++ tipos.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="_css/estilo.css"/>
+	<title></title>
+</head>
+<body>
+<div>
+	<?php
+		$n = 4;
+		$no = "Janilson";
+		echo $no;
+	?>
+</div>
+</body>
+</html>
